dc66be528e5a2ccbefcd0b71564c3b20286fcd67
Edwin Carrasquillo <ecarrasquillo@Edwins-MacBook-Pro.local>
2017-01-17 22:54:58 -0500

    First model for courses; created class & attributes

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
