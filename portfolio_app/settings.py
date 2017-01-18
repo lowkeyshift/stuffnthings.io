56edf9bc4422579f4ffcef0f5db3a0c77d9346da
root <root@portfolio.localdomain>
2017-01-18 03:23:33 +0000

    created courses django app

--- portfolio_app/settings.py
+++ portfolio_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_app',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
